dd2fda06dd13b3e3943f5f079af777dc1caf6467
Gal Peer <gal.peer@post.idc.ac.il>
2022-08-12 14:16:30 +0300

    improve home

---
+++ client/src/components/home-page/home-component-styles.js
@@ -0,0 +1,19 @@
+export const styles = {
+ImageCard: {
+    width: "80%",
+    backgroundColor: "white",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    marginBottom: "25px",
+  },
+  
+ImageTitle: {
+    textAlign: "center",
+    padding: "10px 20px"
+  },
+ListContainer: {
+    width: "500px",
+    height: "500px",
+    display: "flex",
+    justifyContent: "center"
+  }
+};
--- client/src/components/home-page/home-component.js
+++ client/src/components/home-page/home-component.js
@@ -3,11 +3,52 @@ import { NavigationComponent } from "../navigation-bar/navigation-component";
 import { useParams, useLocation  } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { LoginComponent } from "../login-page/login-component";
+import { styles } from "./home-component-styles";
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+
 
 export function HomeComponent(props) {
     const navigate = useNavigate();
     const {state} = useLocation(); // state holds the user data(so we can retrieve its cart)
-   
+   const data =     [   {name: "ball", img: "https://cdn.shopify.com/s/files/1/0712/4751/products/SMA-01_2000x.jpg?v=1629409085", price: 30}, 
+                        {name: "gloves", img: "https://media.4rgos.it/s/Argos/9203547_R_SET?w=270&h=270&qlt=75&fmt.jpeg.interlaced=true", price: 10},
+                        {name: "shirt", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTogI09XQ9B1lFriTleu4E9k-AOkuU_YrllYg&usqp=CAU", price: 50},
+                        {name: "shoes", img: "https://canary.contestimg.wish.com/api/webimage/5d89832baaf8240829e721e1-large.jpg?cache_buster=1ab0b6eaaa4634cc9fe3cdea410c3c86", price: 300}
+                    ];
+    
+
+    const listItems = <ImageList sx={{ width: 500, height: 450, marginLeft:"500px" }}>
+      <ImageListItem key="Subheader" cols={2}>
+        <ListSubheader component="div">Products</ListSubheader>
+      </ImageListItem>
+      {data.map((product) => (
+        <ImageListItem key={product.img}>
+          <img
+            src={`${product.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${product.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={product.name}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={product.name}
+            subtitle={product.price}
+            actionIcon={
+              <IconButton
+                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                aria-label={`info about ${product.name}`}
+              >
+                <InfoIcon />
+              </IconButton>
+            }
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
     //const homeData = fetchHomeData();
 
     // const fetchHomeDate = async () => {
@@ -30,6 +71,7 @@ export function HomeComponent(props) {
     return (
         //need to map list of data and show the products
         <div>
+            {listItems}
             <NavigationComponent />
         </div>
     );
