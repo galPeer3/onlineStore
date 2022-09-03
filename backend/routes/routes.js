5b25fff52f28cd04046856115268b0513da4ad09
gilDagan <gildagan@icloud.com>
2022-09-03 15:31:59 +0300

    deleted readme

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,11 +2,11 @@ const Router = require('express');
 const routes = new Router();
 const productsRouter = require('./products') ;
 const userRouter = require('./user');
-const readme = require('readme.html');
+// const readme = require('readme.html');
 
 routes.use('/user', userRouter);
 routes.use('/products', productsRouter);
-routes.use('/readme.html', readme);
+// routes.use('/readme.html', readme);
 
 
 module.exports = routes;
